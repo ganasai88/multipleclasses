d3335997ad91ac3f999255f79a4eec464b4360ae
Ganasai Palakurthi <39452579+ganasai88@users.noreply.github.com>
2024-06-25 16:55:20 -0400

    Update QueryExecutorTemplate.scala

--- src/main/scala/template/QueryExecutorTemplate.scala
+++ src/main/scala/template/QueryExecutorTemplate.scala
@@ -17,31 +17,51 @@ class QueryExecutorTemplate {
 
   def process(configFilePath: String):Unit = {
 
-    val spark = SparkSession.builder()
-      .appName("Spark Pipeline Example")
-      .master("local[*]")
-      .getOrCreate()
 
     val configFilePath = "C:\\Users\\ganas\\NewScala\\multipleclasses\\src\\main\\scala\\config.json"
 
     val config = ConfigAndJsonParser.parseConfigFile(configFilePath)
 
-    log.info("****************** Processing Input File Start *************************")
-    val inputProcessor = new InputProcessor(spark)
-    val tableDFs = inputProcessor.process(config)
-    log.info("****************** Processing Input File End *************************")
+    config.file_input.foreach { fileInputIterator =>
+      val sourceType = fileInputIterator.source_type
+      // Initialize Spark session based on source type
+      val spark = sourceType match {
+        case "S3Bucket" =>
+          SparkSession.builder()
+            .appName("Spark Pipeline Example")
+            .master("local[*]") // Adjust as needed
+            .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.2,com.amazonaws:aws-java-sdk-pom:1.12.365")
+            .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
+            .config("spark.hadoop.fs.s3a.access.key", "")
+            .config("spark.hadoop.fs.s3a.secret.key", "")
+            .getOrCreate()
 
-    log.info("****************** Processing Query Start *************************")
-    val transformProcessor = new TransformProcessor(spark)
-    val resultDFs = transformProcessor.process(config)
-    log.info("****************** Processing Query End *************************")
+        case "local" =>
+          SparkSession.builder()
+            .appName("Spark Pipeline Example")
+            .master("local[*]") // Adjust as needed
+            .getOrCreate()
 
-    log.info("****************** Output Dataframe to File Start *************************")
-    val outputProcessor = new OutputProcessor(spark)
-    outputProcessor.process(config)
-    log.info("****************** Output Dataframe to File End *************************")
+        case _ =>
+          throw new IllegalArgumentException(s"Unsupported source type: $sourceType")
+      }
 
-    spark.stop()
+      log.info("****************** Processing Input File Start *************************")
+      val inputProcessor = new InputProcessor(spark)
+      val tableDFs = inputProcessor.process(config)
+      log.info("****************** Processing Input File End *************************")
 
+      log.info("****************** Processing Query Start *************************")
+      val transformProcessor = new TransformProcessor(spark)
+      val resultDFs = transformProcessor.process(config)
+      log.info("****************** Processing Query End *************************")
+
+      log.info("****************** Output Dataframe to File Start *************************")
+      val outputProcessor = new OutputProcessor(spark)
+      outputProcessor.process(config)
+      log.info("****************** Output Dataframe to File End *************************")
+
+      spark.stop()
+    }
   }
 }
